84c8eb4b6a0efe8f1f0704a47fd784aaba908614
Rubén <ruben@MacBook-Pro-de-Ruben.local>
2025-06-08 16:43:09 +0200

    add argocd-apps values

--- argocd-install/script.sh
+++ argocd-install/script.sh
@@ -2,4 +2,4 @@ microk8s.helm repo add argo https://argoproj.github.io/argo-helm
 
 microk8s.helm install --create-namespace -n argocd my-argo-cd argo/argo-cd --version 8.0.15
 
-microk8s.helm install -n argocd my-argocd-apps argo/argocd-apps --version 2.0.2 -f values-apps.yaml
+microk8s.helm install -n argocd my-argocd-apps argo/argocd-apps --version 2.0.2 -f values-app.yaml
