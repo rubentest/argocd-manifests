b3f0acdad2193cdc0903039a5caf6f949d71cd3f
Rubén <ruben@MacBook-Pro-de-Ruben.local>
2025-06-08 16:39:08 +0200

    add argocd-apps values

--- argocd-install/script.sh
+++ argocd-install/script.sh
@@ -2,3 +2,4 @@ helm repo add argo https://argoproj.github.io/argo-helm
 
 helm install my-argo-cd argo/argo-cd --version 8.0.15
 
+helm install my-argocd-apps argo/argocd-apps --version 2.0.2 -f values-apps.yaml
